88e12ae565dd7451c97b46291f42826a5fdaa795
buffan <andrew.t.buff@gmail.com>
2016-10-15 22:57:47 -0400

    Now queries for users instead of hashtags

--- GetTweets.py
+++ GetTweets.py
@@ -19,39 +19,34 @@ def read_tokens(tokens_file):
     return tokens
 
 
-def search_and_write(query_text):
+def search_and_write(user):
     """
-    Queries twitter for the given string
-    Inputs: query_text - Text for which twython should query twitter
+    Queries twitter for given user's tweets. Records tweet, retweets, and favorites
+    Inputs: user - User for which twython should query twitter
     """
     tokens = read_tokens("apiKeys.txt")
     twitter = Twython(**tokens)
     
     try:
-        search_results = twitter.search(q=query_text, count=100, 
-                                        result_type='mixed')
+        search_results = twitter.get_user_timeline(screen_name=user, include_rts=False, exclude_replies=True, count=200)
     except TwythonError as e:
       print(e)
 
-    save_file = query_text[1:] + ".txt"
+    save_file = user + ".txt"
     directory = "tweets"
     save_file = join(directory, save_file)
     print(save_file)
     with open(save_file, 'w') as f:
-        for tweet in search_results['statuses']:
-            # Filter tweets with newlines so tweets can be stored in file 
-            # through newlines
+        for tweet in search_results:
             if '\n' not in tweet['text']:
                 f.write(tweet['text'].encode('utf-8') + '\n')
-                f.write("{} {}\n".format(tweet['retweet_count'], 
-                        tweet['favorite_count']))
-
+                f.write('{} {}\n'.format(tweet['retweet_count'], tweet['favorite_count']))
 
 def main():
-    query_text = "#ImWithHer"
-    search_and_write(query_text)
-    query_text = "#TrumpPence16"
-    search_and_write(query_text)
+    user = "HillaryClinton"
+    search_and_write(user)
+    user = "realDonaldTrump"
+    search_and_write(user)
 
 
 
